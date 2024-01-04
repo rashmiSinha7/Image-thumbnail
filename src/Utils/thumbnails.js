f1c9bf2e2e7a977622a8fca28e8de82da200f973
Rashmi <rashmi.sinha1st@gmail.com>
2024-01-04 23:04:21 +0530

    update

--- src/Utils/thumbnails.js
+++ src/Utils/thumbnails.js
@@ -4,14 +4,14 @@ import tb3 from "./tb/tb3.jpg";
 import tb4 from "./tb/tb4.jpg";
 import tb5 from "./tb/tb5.jpg";
 import tb6 from "./tb/tb6.jpg";
-import tb7 from "./tb/tb7.jpg";
+
 import img1 from "./img/img1.jpg";
 import img2 from "./img/img2.jpg";
 import img3 from "./img/img3.jpg";
 import img4 from "./img/img4.jpg";
 import img5 from "./img/img5.jpg";
 import img6 from "./img/img6.jpg";
-import img7 from "./img/img7.jpg";
+
 
 export const images = [
   { id: 0, tb: tb1, img: img1 },
@@ -20,5 +20,4 @@ export const images = [
   { id: 3, tb: tb4, img: img4 },
   { id: 4, tb: tb5, img: img5 },
   { id: 5, tb: tb6, img: img6 },
-  { id: 6, tb: tb7, img: img7 },
 ];
